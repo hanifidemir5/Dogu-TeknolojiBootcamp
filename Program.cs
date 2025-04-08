122f64fcc6387256ddd86701e7ca7571d04977e8
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-04-08 13:11:28 +0300

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using BlogApp.Data.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>{
+    options.AccessDeniedPath = "/Users/AccessDenied";
+    options.LoginPath = "/Users/Login";
+    options.LogoutPath = "/Users/Logout";
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -13,13 +22,33 @@ builder.Services.AddDbContext<BlogContext>(options =>
 
 builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 builder.Services.AddScoped<ITagRepository, EfTagRepository>();
+builder.Services.AddScoped<ICommentRepository, EfCommentRepository>();
+builder.Services.AddScoped<IUserRepository, EfUserRepository>();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 SeedData.TestVerileriniDoldur(app);
 
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "post_details",
+    pattern: "blog/{url}",
+    defaults: new { controller = "Posts", action = "Details" }
+);
+
+app.MapControllerRoute(
+    name: "posts_by_tag",
+    pattern: "blogs/tag/{tag}",
+    defaults: new { controller = "Posts", action = "Index" }
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Posts}/{action=Index}/{id?}"
+);
 
 app.Run();
