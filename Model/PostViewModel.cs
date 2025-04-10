d958ca7806ed3b9082c2c0abd61164aa5d7ed475
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-04-10 23:07:47 +0300

    Pagination changes

--- Model/PostViewModel.cs
+++ Model/PostViewModel.cs
@@ -1,9 +1,12 @@
 using BlogApp.Entity;
 
 namespace BlogApp.Models{
-    public class PostViewModel{
-    public List<Post> Posts { get; set; } = new();
-    public List<List<Tag>> Tags { get; set; } = new();  // This will hold tags for each post
-}
+    public class PostViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public Dictionary<int, List<Tag>> PostTags { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
 
 }
